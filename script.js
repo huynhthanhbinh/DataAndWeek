17c888546d844ee533dc3b7ccbd36a53fe563c21
Richard <huynhthanhbinh1998@gmail.com>
2019-04-02 18:02:09 +0700

    Init

---
+++ script.js
@@ -0,0 +1,9 @@
+// Ctrl Alt N to runcode
+var today = new Date();
+console.log(today);
+var dd = today.getDate();
+console.log("day is", dd);
+var mm = today.getMonth() + 1;
+console.log("month is", mm);
+var yyyy = today.getFullYear();
+console.log("year is", yyyy);
